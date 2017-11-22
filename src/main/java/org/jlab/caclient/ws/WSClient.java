37072950310fe50b65e7e45d94bdd08efd82fd0f
slominskir <Ryan.Slominski@gmail.com>
2017-11-22 16:26:06 -0500

    Gradle build support

---
+++ src/main/java/org/jlab/caclient/ws/WSClient.java
@@ -0,0 +1,59 @@
+package org.jlab.caclient.ws;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import javax.websocket.CloseReason;
+import javax.websocket.ContainerProvider;
+import javax.websocket.DeploymentException;
+import javax.websocket.Endpoint;
+import javax.websocket.EndpointConfig;
+import javax.websocket.Session;
+import javax.websocket.WebSocketContainer;
+import org.jlab.caclient.CAClient;
+import org.jlab.caclient.ChannelGroup;
+
+/**
+ *
+ * @author slominskir
+ */
+public class WSClient implements CAClient {
+
+    private final Session session;
+    private final WSContext context;
+
+    public WSClient() throws URISyntaxException, DeploymentException, IOException {
+        final WebSocketContainer container = ContainerProvider.getWebSocketContainer();
+
+        session = container.connectToServer(new Endpoint() {
+            @Override
+            public void onOpen(Session sn, EndpointConfig ec) {
+                System.out.println("Opened!");
+            }
+
+            @Override
+            public void onClose(Session sn, CloseReason reason) {
+                System.out.println("Closed: " + reason);
+            }
+
+            @Override
+            public void onError(Session sn, Throwable thrwbl) {
+                System.err.println("Error!");
+                thrwbl.printStackTrace();
+            }
+        }, new URI("ws://127.0.0.1:8084/epics2web/monitor"));
+
+        context = new WSContext(session);
+    }
+
+    @Override
+    public ChannelGroup create(String[] channelNames) {
+        return WSChannelGroup.create(context, channelNames);
+    }
+
+    @Override
+    public void close() throws Exception {
+        session.close();
+    }
+
+}
