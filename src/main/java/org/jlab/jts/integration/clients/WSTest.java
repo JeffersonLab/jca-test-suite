a9f20a6a3dab2addb21feb34b39243cda9ed9279
slominskir <Ryan.Slominski@gmail.com>
2017-11-29 15:48:27 -0500

    Cleanup

--- src/main/java/org/jlab/jts/integration/clients/WSTest.java
+++ src/main/java/org/jlab/jts/integration/clients/WSTest.java
@@ -3,6 +3,7 @@ package org.jlab.jts.integration.clients;
 import org.jlab.jts.caclient.ws.WSClient;
 import org.jlab.jts.integration.TestCase;
 import org.jlab.jts.integration.testcase.HelloWorldTestCase;
+import org.jlab.jts.integration.testcase.HighConcurrencyTestCase;
 
 /**
  *
@@ -11,7 +12,7 @@ import org.jlab.jts.integration.testcase.HelloWorldTestCase;
 public class WSTest {
 
     public static void main(String[] args) throws Exception {
-        try (TestCase test = new HelloWorldTestCase(new WSClient())) {
+        try (TestCase test = new HighConcurrencyTestCase(new WSClient())) {
             test.doTest();
         }
         
